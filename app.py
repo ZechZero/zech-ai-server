8241e694cc130d5d97cc9a5a5582a5b7eddbba5f
ZechZero <randiganteng2333@gmail.com>
2026-07-28 20:25:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from routes.hd import router as hd_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Zech AI Server",
+    version="1.0.0"
+)
+
+app.include_router(hd_router, prefix="/hd")
 
 @app.get("/")
 def home():
     return {
         "name": "Zech AI Server",
-        "version": "1.0.0",
         "status": "online"
     }
